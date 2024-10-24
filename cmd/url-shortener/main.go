989b3f958cf4661ef486d833017000518773c8f9
Artem An <Arti An>
2024-10-24 18:23:12 +0300

    Add redirect and delete handlers

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/delete"
+	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/http-server/handlers/url/save"
 	"url-shortener/internal/lib/logger/sl"
 
@@ -76,6 +78,8 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/url", save.New(log, storage))
+	router.Get("/{alias}", redirect.New(log, storage))
+	router.Delete("/{alias}", delete.New(log, storage))
 
 	log.Info("starting server", slog.String("adress", conf.Adress))
 
---
+++ internal/http-server/handlers/redirect/redirect_test.go
@@ -0,0 +1,48 @@
+package redirect_test
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"url-shortener/internal/http-server/handlers/redirect"
+	slogdiscard "url-shortener/internal/lib/logger/handlers"
+	mysql "url-shortener/internal/storage/MySQL"
+)
+
+func TestSaveHandler(t *testing.T) {
+	cases := []struct {
+		name      string
+		alias     string
+		url       string
+		respError string
+		mockError error
+	}{
+		{
+			name:  "Success",
+			alias: "test_alias",
+			url:   "https://www.google.com/",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			storage, err := mysql.NewStorage()
+
+			r := chi.NewRouter()
+			r.Get("/{alias}", redirect.New(slogdiscard.NewDiscardLogger(), storage))
+
+			ts := httptest.NewServer(r)
+			defer ts.Close()
+
+			redirectedToURL, err := api.GetRedirect(ts.URL + "/" + tc.alias)
+			require.NoError(t, err)
+
+			// Check the final URL after redirection.
+			assert.Equal(t, tc.url, redirectedToURL)
+		})
+	}
+}
